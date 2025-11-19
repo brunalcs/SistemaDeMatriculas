2998cfa12650ae421e5b5fa272e5641f205a2fde
Bruna Lucena <brunalucena.prog@gmail.com>
2025-11-18 22:01:41 -0300

    criacao classe Aluno

---
+++ src/main/java/br/com/matriculas/api/entidades/Aluno.java
@@ -0,0 +1,49 @@
+package br.com.matriculas.api.entidades;
+
+public class Aluno {
+	private String nome;
+	private String matricula;
+	private String curso;
+	private Double ira;
+	
+	public Aluno() {}
+	
+	public Aluno(String nome, String matricula, String curso, Double ira) {
+		this.nome=nome;
+		this.matricula=matricula;
+		this.curso=curso;
+		this.ira=ira;
+	}
+	
+	public String getName() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome=nome;
+	}
+	
+	
+	 public String getMatricula() {
+	     return matricula;
+	 }
+
+	 public void setMatricula(String matricula) {
+	     this.matricula = matricula;
+	 }
+
+    public String getCurso() {
+	     return curso;
+    }
+    public void setCurso(String curso) {
+	     this.curso = curso;
+	}
+
+    public Double getIra() {
+	     return ira;
+	}
+    
+    public void setIra(Double ira) {
+	    this.ira = ira;
+    }
+}
